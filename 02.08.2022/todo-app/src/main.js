05a0b927f1c3b2e97e3f79d8c259aa2069f868ff
Recep <recepcengiz_96@outlook.com>
2024-09-02 10:17:12 +0300

    yorum satırı eklendi

--- 02.08.2022/todo-app/src/main.js
+++ 02.08.2022/todo-app/src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue' // bir değişken ya da method export ettiğimizde süslü parantez kullanırız.
+import { createApp } from 'vue' // bir değişken ya da method export ettiğimizde süslü parantez kullanırız. Aslında şu mana var: Biz birden fazla şey(değişken,metot vs) dışarı aktarmak istiyorsak export ile dosyadan dışarı aktarırız ve çağırdığımız sayfada da import ederken o dosyada birden fazla şey olduğu için bu sayfaya dahil edeceğimiz şeyler bunlar diye importta süslü parantez içinde şunları getir diye belirtiriz diğerlerinin arasından
+ 
 import App from './App.vue'
 
 //export const eventBus=new Vue();
