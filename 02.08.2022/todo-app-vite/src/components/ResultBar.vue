98014c2822a26d12b3d0937dec2837121ff03b89
Recep <recepcengiz_96@outlook.com>
2024-09-02 11:03:35 +0300

    defineProps eklendi

--- 02.08.2022/todo-app-vite/src/components/ResultBar.vue
+++ 02.08.2022/todo-app-vite/src/components/ResultBar.vue
@@ -1,15 +1,22 @@
 <template>
     <div class="my-2 d-flex justify-content-between">
         <span class="text-success">Görev sayısı:  {{ len }} </span>
+        <span class="text-success">Görev sayısı:  {{ length_ }} </span>
     </div>
 </template>
 
 
 <script setup>
-import { computed, inject } from 'vue';
+import { computed, inject, defineProps } from 'vue';
 
+defineProps({
+    todoList:{
+        type:Array
+    }
+})
+const length_= todoList.length || 0;
 
 const provideData=inject("provideData");
-
 const len=computed(()=>provideData.todoList?.length || 0);
+
 </script>
